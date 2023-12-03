39bab97ba39df88ce4d12e6fb01b33aab084071b
yuichiro_shimura <yuichiro.shimura.05@gmail.com>
2023-12-03 11:42:42 +0900

    [Update] add school_deviation_average

--- highschool_search.py
+++ highschool_search.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 school_name = []
 school_summary = []
 school_deviation = []
+school_deviation_average = []
 school_review = []
 
 # ページ数の初期値
@@ -16,6 +17,7 @@ page_num = page_num_init
 location = "名古屋市"
 except_word_1 = "女子校"
 except_word_2 = "中高一貫校"
+except_word_3 = "閉校"
 url = "https://www.minkou.jp/hischool/search/pref=aichi/page={}/".format(page_num_init)
 
 # Webページを取得
@@ -69,14 +71,32 @@ while page_num <= total_page_num:
             continue
         
         # 学校名を取得
-        school_name.append(result.find("a").text)
-
+        school_name_element = result.find("a").text
+        if except_word_3 in school_name_element:
+            continue
+        
+        else:
+            school_name.append(result.find("a").text)
+        
         # 学校の概要を取得
         school_summary.append(result.find("span").text)
 
         # 偏差値を取得
         school_deviation.append(result.find("dd").text)
-    
+        
+        # 偏差値(中央値)を算出
+        dev_range_str = result.find("dd").text
+        dev_range_str_join = "".join(dev_range_str).split("-")
+        dev_range_list = []
+        if not dev_range_str[0] == "-":
+            print(f"{school_name_element}:True")
+            for dev_range_str_element in dev_range_str_join:
+                dev_range_list.append(int(dev_range_str_element))
+        else:
+            print(f"{school_name_element}:False")
+            dev_range_list.append(1)
+        school_deviation_average.append(sum(dev_range_list)/len(dev_range_list))
+        
         # 口コミの点数を取得
         school_review.append(result.find("div", class_="mod-listSearch-review").find("a").text)
     
@@ -86,7 +106,7 @@ while page_num <= total_page_num:
     page_num += 1
 
 print(f"ヒット件数:{match_num}")
-school_data = {"学校名":school_name, "概要":school_summary, "偏差値":school_deviation, "評判":school_review}
+school_data = {"学校名":school_name, "概要":school_summary, "偏差値(範囲)":school_deviation, "偏差値(中央値)":school_deviation_average, "評判":school_review}
 school_data_table = pd.DataFrame(school_data)
 print(school_data_table)
 school_data_table.to_csv("search_results.csv")
